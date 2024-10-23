7de37a27fa3ceff0349381d7dd88c81fa2285b12
Loice Khoene <82638055+Loicemary@users.noreply.github.com>
2024-10-23 22:43:56 +0300

    feat: set selected country

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { closeDropdown } from '@finsweet/ts-utils';
+
 import { fetchCountries, fetchIpInfo } from '$utils/api';
 import type { Country, IpInfo } from '$utils/types';
 
@@ -18,12 +20,18 @@ window.Webflow.push(async () => {
 });
 
 const populateCountryList = (countries: Country[]) => {
-  const prefixListWrapperElement = document.querySelector('#w-dropdown-list-0 > div');
+  const prefixListWrapperElement = document.querySelector<HTMLDivElement>('#w-dropdown-list-0');
   if (!prefixListWrapperElement) {
     console.error('Prefix list element not found');
     return;
   }
-  const prefixListItemElement = prefixListWrapperElement.querySelector('a');
+  const prefixListElement = prefixListWrapperElement.querySelector('div');
+  if (!prefixListElement) {
+    console.error('Prefix list element not found');
+    return;
+  }
+
+  const prefixListItemElement = prefixListElement.querySelector('a');
 
   if (!prefixListItemElement) {
     console.error('Prefix list item element not found');
@@ -49,7 +57,24 @@ const populateCountryList = (countries: Country[]) => {
 
     itemFlagElement.src = country.flags.png;
     itemValueElement.textContent = country.cca2;
-    prefixListWrapperElement.appendChild(prefixListItemClone);
+    prefixListElement.appendChild(prefixListItemClone);
+
+    // add event listener to the prefix list item
+    prefixListItemClone.addEventListener('click', () => {
+      // close the dropdown and focus the dropdown toggle after selecting a country
+      closeDropdown(prefixListWrapperElement, true);
+      // set the value of the input[name=countryCode] to the country code
+      const countryCodeInputElement =
+        document.querySelector<HTMLInputElement>('input[name=countryCode]');
+      if (!countryCodeInputElement) {
+        console.error('Country code input element not found');
+        return;
+      }
+      countryCodeInputElement.value = country.cca2;
+
+      // Change dropdown toggle text to the country idd root and suffix
+      setSelectedValueOnDropdownToggle(country);
+    });
   });
 
   // remove the original prefix list item template
@@ -82,4 +107,26 @@ const setDefaultSelectedValue = (userLocation: IpInfo, countries: Country[]) => 
 
   itemFlagElement.src = currentCountry.flags.png;
   itemValueElement.textContent = currentCountry.idd.root + currentCountry.idd.suffixes[0];
+};
+
+// set selected value on dropdown toggle
+const setSelectedValueOnDropdownToggle = (country: Country) => {
+  const dropdownToggleElement = document.querySelector('#w-dropdown-toggle-0');
+  if (!dropdownToggleElement) {
+    console.error('Dropdown toggle element not found');
+    return;
+  }
+  // flag element
+  const itemFlagElement =
+    dropdownToggleElement.querySelector<HTMLImageElement>('[data-element="flag"]');
+  // value element
+  const itemValueElement = dropdownToggleElement.querySelector('[data-element="value"]');
+
+  if (!itemFlagElement || !itemValueElement) {
+    console.error('Prefix list item element not found');
+    return;
+  }
+
+  itemFlagElement.src = country.flags.png;
+  itemValueElement.textContent = country.idd.root + country.idd.suffixes[0];
 };
